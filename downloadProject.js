bd2fb8089183505ee2dfc3d1eab1d894989b3e80
fespadea <fespadearocks@gmail.com>
2024-06-29 13:40:45 +0200

    fixed small problems and added image and project downloading

--- downloadProject.js
+++ downloadProject.js
@@ -1,16 +1,31 @@
-const SBDL = require('@turbowarp/sbdl');
+const SBDL = require("@turbowarp/sbdl");
+const fs = require("node:fs");
 
-const options = {
+const downloadProjects = async function() {
+  const options = {
     // May be called periodically with progress updates.
     onProgress: (type, loaded, total) => {
-        // type is 'metadata', 'project', 'assets', or 'compress'
-        console.log(type, loaded / total);
-    }
-};
+      // type is 'metadata', 'project', 'assets', or 'compress'
+      console.log(type, loaded / total);
+    },
+  };
+  
+  const authData = require("./scratchAuthenticationToken.json");
+  const projectsToDownload = require("./" + authData["username"] +
+    " projects (including descended).json");
+  const numProjects = Object.keys(projectsToDownload).length
+  let projectCounter = 0;
 
-module.exports =  async function downloadProject() {
-    const project = await SBDL.downloadProjectFromID('1582245', options);
-    console.log(project)
-    c
-    project
+  for (const projectID of Object.keys(projectsToDownload)) {
+    const project = await SBDL.downloadProjectFromID(projectID, options);
+    for (const saveLocation of projectsToDownload[projectID]) {
+      fs.writeFile(saveLocation + project.title + "." + project.type, Buffer.from(project.arrayBuffer), { flag: 'w' }, (err) => {
+        if (err) console.log(err);
+      });
+    }
+    console.log(`${++projectCounter} / ${numProjects}: ${project.title}`)
   }
+}
+
+downloadProjects();
+console.log("Done")
